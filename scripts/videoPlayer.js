7525d92be597fe2a8e7d5b930d4f081467064b6f
Vladislav <infohunterr@gmail.com>
2020-12-24 13:33:27 +0600

    almost finished

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,3 +1,5 @@
+import { addZero } from "./supScript.js";
+
 export const videoPlayerInit = () => {
   const videoPlayer = document.querySelector(".video-player");
   const videoButtonPlay = document.querySelector(".video-button__play");
@@ -31,7 +33,13 @@ export const videoPlayerInit = () => {
     videoPlayer.currentTime = 0;
   };
 
-  const addZero = (n) => (n < 10 ? "0" + n : n);
+  /* const soundOff = () => {
+    const activeClass = videoPlayer.closest('active');
+
+    if (activeClass) {
+
+    }
+  }; */
 
   videoPlayer.addEventListener("click", togglePlay);
   videoButtonPlay.addEventListener("click", togglePlay);
